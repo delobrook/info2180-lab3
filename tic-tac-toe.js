bbb555acf89c0cd0a511502438bd14b82acd6bfa
Delano tracey <delanotracey@gmail.com>
2020-10-09 17:00:39 -0500

    exercise 3 update

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,22 +4,21 @@ window.onload=function(){
     var squares=board.children;
     for(let i=0;i<board.children.length;i++){
         squares[i].classList.add("square");
-        squares[i].append(document.createElement("div"));
         squares[i].onclick=function(){xoro(i)} ;
         squares[i].onmouseover=function(){ccmover(i)};
         squares[i].onmouseout=function(){ccmout(i)};
     }
     function xoro(i){
-        if(squares[i].firstChild.firstChild==null){
-            squares[i].firstChild.textContent="X";
-            squares[i].firstChild.classList.add("X");
-        }else if(squares[i].firstChild.textContent=="X"){
-            squares[i].firstChild.textContent="O";
-            squares[i].firstChild.classList.remove("X");
-            squares[i].firstChild.classList.add("O");
+        if(squares[i].firstChild==null){
+            squares[i].textContent="X";
+            squares[i].classList.add("X");
+        }else if(squares[i].textContent=="X"){
+            squares[i].textContent="O";
+            squares[i].classList.remove("X");
+            squares[i].classList.add("O");
         } else if(squares[i].firstChild.textContent=="O"){
-            squares[i].firstChild.textContent=null;
-            squares[i].firstChild.classList.remove("O");
+            squares[i].textContent=null;
+            squares[i].classList.remove("O");
         
         } 
     }
